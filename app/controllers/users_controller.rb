875f768d3216d3f21098e1112e6d2095ad228478
calmdown1987 <calmdown1987@icloud.com>
2022-08-31 12:34:49 +0000

    8.2.2 正しいユーザーであることを要求する

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
   before_action :logged_in_user, only: [:show, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
   
   def show
-    @user = User.find(params[:id])
   end
 
   def new
@@ -21,14 +22,12 @@ class UsersController < ApplicationController
   end
   
   def edit
-    @user = User.find(params[:id])
   end
   
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(user_params)
-    flash[:success] = "ユーザー情報を更新しました。"
-    redirect_to @user
+      flash[:success] = "ユーザー情報を更新しました。"
+      redirect_to @user
     else
       render :edit
     end
@@ -40,10 +39,19 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
     
+    def set_user
+      @user = User.find(params[:id])
+    end
+    
     def logged_in_user
-    unless logged_in?
+     unless logged_in?
       flash[:danger] = "ログインしてください。"
       redirect_to login_url
+     end
     end
+    
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless @user == current_user?(@user)
     end
 end
