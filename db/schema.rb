0ac60d51ac4036322d325d5b195891cbd378ac37
calmdown1987 <calmdown1987@icloud.com>
2022-09-06 01:29:39 +0000

    departmentカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20220905103822) do
+ActiveRecord::Schema.define(version: 20220906012022) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 20220905103822) do
     t.string "remember_digest"
     t.boolean "admin", default: false
     t.string "department"
+    t.datetime "basic_time", default: "2022-09-05 23:00:00"
+    t.datetime "work_time", default: "2022-09-05 22:00:00"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
