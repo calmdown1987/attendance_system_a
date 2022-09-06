43298bcd44739c92c2021bc2a68f04387095320b
calmdown1987 <calmdown1987@icloud.com>
2022-09-06 01:42:02 +0000

    9.2.2 表示に対応しよう

--- db/schema.rb
+++ db/schema.rb
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 20220906012022) do
     t.boolean "admin", default: false
     t.string "department"
     t.datetime "basic_time", default: "2022-09-05 23:00:00"
-    t.datetime "work_time", default: "2022-09-05 22:00:00"
+    t.datetime "work_time", default: "2022-09-05 22:30:00"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
