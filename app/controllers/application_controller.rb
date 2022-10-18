cd2e42d8aec5325ad875a55105e90ef5f14ef3ea
calmdown1987 <calmdown1987@icloud.com>
2022-10-18 11:21:09 +0000

    first commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
     @user = User.find(params[:id])
   end
 
-  # ログイン済みのユーザーか確認します。
+  # ログイン済みのユーザーか確認するためです。
   def logged_in_user
     unless logged_in?
       store_location
